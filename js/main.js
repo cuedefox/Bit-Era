a511bde0cc895356b7aac434863a382a68e72cad
Rodrigo Vergara <rodrigovergara2001@gmail.com>
2022-07-08 06:27:57 -0300

    agrego algo de html y css

---
+++ js/main.js
@@ -0,0 +1,10 @@
+class juego {
+    constructor(nombre, precio, genero, año, desarrolladora) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.genero = genero;
+        this.año = año;
+        this.desarrolladora = desarrolladora;
+    }
+}
+let juegos = [];
