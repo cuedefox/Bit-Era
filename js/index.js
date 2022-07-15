895887d4e79ffe01af9a1bc200d639e63f9a7924
Rodrigo Vergara <rodrigovergara2001@gmail.com>
2022-07-15 16:34:13 -0300

    termino de agregar productos

--- js/index.js
+++ js/index.js
@@ -18,16 +18,16 @@ class producto {
 }
 
 function inicializarProductos() {
-    productos.push(new producto(0, "Nintendo Switch", "consola", 119999.99, 2017, "consola", "Nintendo", 0));
-    productos.push(new producto(1, "PS1", "consola", 29999.99, 1994, "consola", "Sony", 0));
-    productos.push(new producto(2, "Game boy color", "consola", 82399.99, 1998, "consola", "Nintendo", 0));
-    productos.push(new producto(3, "PSP", "consola", 29999.99, 2005, "consola", "Sony", 0));
-    productos.push(new producto(4, "XBOX", "consola", 69999.99, 2001, "consola", "Microsoft", 0));
-    productos.push(new producto(5, "DS Lite", "consola", 19999.99, 2006, "consola", "Nintendo", 0));
-    productos.push(new producto(6, "PS2", "consola", 39999.99, 2000, "consola", "Sony", 0));
-    productos.push(new producto(7, "PS3", "consola", 54999.99, 2006, "consola", "Sony", 0));
-    productos.push(new producto(8, "PS4", "consola", 89999.99, 2013, "consola", "Sony", 0));
-    productos.push(new producto(9, "PS5", "consola", 249999.99, 2020, "consola", "Sony", 0));
+    productos.push(new producto(0, "Nintendo Switch", "consola", 119999.99, 2017, "consola", "Nintendo", 143));
+    productos.push(new producto(1, "PS 1", "consola", 29999.99, 1994, "consola", "Sony", 1));
+    productos.push(new producto(2, "Game boy color", "consola", 82399.99, 1998, "consola", "Nintendo", 32));
+    productos.push(new producto(3, "PSP", "consola", 29999.99, 2005, "consola", "Sony", 115));
+    productos.push(new producto(4, "XBOX", "consola", 69999.99, 2001, "consola", "Microsoft", 3));
+    productos.push(new producto(5, "DS Lite", "consola", 19999.99, 2006, "consola", "Nintendo", 7));
+    productos.push(new producto(6, "PS 2", "consola", 39999.99, 2000, "consola", "Sony", 298));
+    productos.push(new producto(7, "PS 3", "consola", 54999.99, 2006, "consola", "Sony", 2));
+    productos.push(new producto(8, "PS 4", "consola", 89999.99, 2013, "consola", "Sony", 75));
+    productos.push(new producto(9, "PS 5", "consola", 249999.99, 2020, "consola", "Sony", 38));
     productos.push(new producto(10, "Metal Gear", "juego", 299.99, 1987, "sigilo", "Konami", 0));
     productos.push(new producto(11, "Metal Gear 2: solid snake", "juego", 299.99, 1990, "sigilo", "Konami", 0));
     productos.push(new producto(12, "Metal Gear solid", "juego", 1199.99, 1998, "sigilo", "Konami", 0));
@@ -39,23 +39,51 @@ function inicializarProductos() {
     productos.push(new producto(18, "Metal Gear rising: revengance", "juego", 999.99, 2013, "accion", "Konami", 11));
     productos.push(new producto(19, "Metal Gear solid 5: definitive experience", "juego", 1999.99, 2016, "sigilo", "Konami", 3));
     productos.push(new producto(20, "La leyenda de Mateo", "juego", 0.00, 2020, "rpg", "cuede games", 460));
+    productos.push(new producto(21, "GTA V", "juego", 599.99, 2013, "accion", "rockstar", 204));
+    productos.push(new producto(22, "COD BO1", "juego", 299.99, 2010, "accion", "treyarch", 115));
+    productos.push(new producto(23, "Final Fantasy", "juego", 299.99, 2007, "rpg", "square enix", 32));
+    productos.push(new producto(24, "Final Fantasy 2", "juego", 299.99, 2008, "rpg", "square enix", 4));
+    productos.push(new producto(25, "Star Wars 3", "juego", 549.99, 2005, "accion", "lucas arts", 47));
+    productos.push(new producto(26, "The last of us", "juego", 849.99, 2013, "accion", "naughty dog", 200));
+    productos.push(new producto(27, "The last of us 2", "juego", 2999.99, 2020, "accion", "naughty dog", 53));
+    productos.push(new producto(28, "Crash Bandicoot Nsane trilogy", "juego", 1849.99, 2017, "plataformas", "naughty dog", 23));
+    productos.push(new producto(29, "Silent Hill 2", "juego", 649.99, 2001, "terror", "konami", 66));
+    productos.push(new producto(30, "Star Wars jedi fallen order", "juego", 2849.99, 2019, "accion", "electronic arts", 2));
 }
 
 function filtrarProductos(tipo, dom, array, seccion) {
-    if(seccion != "destacados") {
+    if(seccion != 3) {
         for(let i = 0; i < productos.length; i++) {
             if(productos[i].tipo == tipo) {
              array.push(productos[i]);
             }
         }
     }
-    if(seccion === "ultimos lanzamientos") {
+    if(seccion == 3) {
+        for(let i = 0; i < productos.length; i++) {
+            array.push(productos[i]);
+        }
+    }
+    if(seccion == 1) {
         array.sort((a, b) => {
             if(a.año < b.año) {
-                return -1;
+                return 1;
             }
             if(a.año > b.año) {
+                return -1;
+            }
+            else {
+                return 0;
+            }
+        })
+    }
+    if(seccion == 0 || seccion >= 2) {
+        array.sort((a, b) => {
+            if(a.ventas < b.ventas) {
                 return 1;
+            }
+            if(a.ventas > b.ventas) {
+                return -1;
             }
             else {
                 return 0;
@@ -66,7 +94,7 @@ function filtrarProductos(tipo, dom, array, seccion) {
         let productoDom = document.createElement("div");
         productoDom.className = "producto";
         productoDom.innerHTML = `<img src="img/productos/${array[i].id}.jpg" alt="imagen ${array[i].nombre}">
-        <h5>${array[i].nombre}</h5><p><span class="precio">${array[i].precio}</span></p>
+        <h5>${array[i].nombre}</h5><p><span class="precio">${array[i].precio}$</span></p>
         <button id="agregar${array[i].id}">Agregar al carrito</button>`;
         dom.appendChild(productoDom);
     }
@@ -76,9 +104,11 @@ function mostrarProductos() {
     let productosConsolas = [];
     let productosJuegosNuevos = [];
     let productosJuegosMasVendidos = [];
-    filtrarProductos("consola", consolasDom, productosConsolas, "consola");
-    filtrarProductos("juego", ultimosLanzamientosDom, productosJuegosNuevos, "ultimos lanzamientos");
-    filtrarProductos("juego", masVendidosDom, productosJuegosMasVendidos, "mas cendidos");
+    let destacados = [];
+    filtrarProductos("consola", consolasDom, productosConsolas, 0);
+    filtrarProductos("juego", ultimosLanzamientosDom, productosJuegosNuevos, 1);
+    filtrarProductos("juego", masVendidosDom, productosJuegosMasVendidos, 2);
+    filtrarProductos("producto", destacadosDom, destacados, 3);
 }
 
 function main() {
